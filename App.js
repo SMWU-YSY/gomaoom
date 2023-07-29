8af7bc72e8f772f7696ada6732763381380b7aed
Nayujin <00nyj@sookmyung.ac.kr>
2023-07-29 17:53:49 +0900

    [FEAT] 첫 화면

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const loginNaver=()=>{
+    console.log('네이버로그인..');
+  };
+  const loginKakao=()=>{
+    console.log('카카오로그인..');
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.background}>
+      <Image source={require('gomaoom/assets/blueTop.png')}/>
+      <View style={styles.container}>
+        <Image source={require('gomaoom/assets/fakeLogo.png')}/>
+        <Text style={styles.desc}>
+          감성글 한두줄. 예를들면, 사랑하는 이에게 마음을 보내봐요. 사랑하는 이의 편지를 움켜쥐어봐요
+        </Text>
+        <View style={styles.btnContainer}>
+          <View style={{marginBottom:20,...styles.loginBtn}}>
+            <TouchableOpacity onPress={loginNaver}>
+              <Text style={{
+                fontSize:30,
+              }}>
+                네이버 로그인
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.loginBtn}>
+            <TouchableOpacity onPress={loginKakao}>
+              <Text style={{
+                fontSize:30,
+              }}>
+                카카오 로그인
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
       <StatusBar style="auto" />
+      <View style={{flexDirection:'row',justifyContent:'flex-end'}}>
+        <Image  source={require('gomaoom/assets/greenBottom.png')}/>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  background:{
+    backgroundColor: '#FFFCF8',
+    flex:1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  desc:{
+    fontSize:25,
+    marginTop:30,
+    marginHorizontal:30,
+  },
+  btnContainer:{
+    // flexDirection:'column',
+    // justifyContent:'flex-end',
+    marginTop:110,
+    // backgroundColor:'pink',
+  },
+  loginBtn:{
+    backgroundColor:'#F1F1F1',
+    paddingHorizontal:30,
+    paddingVertical:7,
+    borderRadius:20,
   },
 });
