61f7e540b20212b50d625ec0b961363db05bbfea
Nayujin <00nyj@sookmyung.ac.kr>
2023-09-25 23:21:35 +0900

    [FIX] 로그인 구조 수정

--- Login.js
+++ Login.js
@@ -1,76 +1,181 @@
+import axios from 'axios';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image,TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Image,TouchableOpacity, TextInput } from 'react-native';
 
-export default function App() {
-    const loginNaver=()=>{
-    console.log('네이버로그인..');
-    };
-    const loginKakao=()=>{
-        console.log('카카오로그인..');
-    };
-    return (
-        <View style={styles.background}>
-        <View style={styles.container}>
-            <Image source={require('gomaoom/assets/fakeLogo.png')}/>
-            <Text style={styles.desc}>
-            감성글 한두줄. 예를들면, 사랑하는 이에게 마음을 보내봐요. 사랑하는 이의 편지를 움켜쥐어봐요
-            </Text>
-            <View style={styles.btnContainer}>
-            <TouchableOpacity onPress={loginNaver}>
+export default function App({navigation}) {
+  const [inputs,setInputs]=useState({
+    id:'',
+    password:'',
+  });  
+  const {id,password}=inputs;
+  // const [accessToken,setAccessToken]=useState('');
+  let accessToken='';
+  const onChange=(keyvalue,e)=>{
+    const {text}=e.nativeEvent
+    setInputs({
+      ...inputs,
+      [keyvalue]:text
+    });
+  };
+  const onLoginClick=async()=>{
+		try {
+		  const response = await axios.post("http://3.34.212.92:8080/api/user/login", 
+		  {	
+				loginId: id,
+				password:password,
+			}
+		  , 
+      {
+        headers: {
+          Accept: 'application/json',
+          "Content-Type": "application/json",
+        }
+      });
+	
+		  console.log(response.data.data[0]);
+
+      accessToken=response.headers.getAuthorization().split(" ");
+      accessToken=accessToken[1];
+      console.log(accessToken);
+      
+		} catch (error) {
+			console.log(error);
+		} 
+	};
+  const gotoRegister=()=>{
+      console.log('회원가입으로 이동');
+      navigation.navigate('sendList');
+  };
+
+  return (
+      <View style={styles.background}>
+      <Image source={require('gomaoom/assets/blueTop.png')}/>
+      
+      <View style={styles.container}>
+        <View style={styles.titleBox}>
+          <Text style={{textAlign:'center'}}>
+            Welcome
+          </Text>
+          <Text style={styles.mainTitle}>
+            고마움
+          </Text>
+        </View>
+        
+        <View style={styles.loginContainer}>
+          <View style={styles.loginInputContainer}>
+            <TextInput 
+              placeholder='아이디' 
+              style={styles.inputText}
+              value={id}
+              onChange={(e)=>{onChange("id",e)}}
+              />
+            
+          </View>
+          <View style={styles.loginInputContainer}>
+            <TextInput 
+              placeholder='비밀번호' 
+              style={styles.inputText}
+              value={password}
+              onChange={(e)=>{onChange("password",e)}}
+              />
+          </View>
+          <TouchableOpacity onPress={onLoginClick}>
             <View style={{marginBottom:20,...styles.loginBtn}}>
                 <Text style={{
                     fontSize:30,
+                    textAlign:'center'
                 }}>
-                    네이버 로그인
+                  로그인
                 </Text>
             </View>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={loginKakao}>
-            <View style={styles.loginBtn}>
-                <Text style={{
-                    fontSize:30,
-                }}>
-                    카카오 로그인
-                </Text>
-            </View>
-            </TouchableOpacity>
-
-            </View>
-        </View>
-        <StatusBar style="auto" />
-        <View style={{flexDirection:'row',justifyContent:'flex-end'}}>
-            <Image  source={require('gomaoom/assets/greenBottom.png')}/>
+          </TouchableOpacity>
         </View>
 
+        <View style={styles.btnContainer}>
+        <TouchableOpacity onPress={gotoRegister}>
+          <Text>
+            아직 회원이 아니신가요?
+          </Text>
+            <Text style={{
+                fontSize:30,
+                textAlign:'center',
+                color:'green'
+            }}>
+                가입하기
+            </Text>
+        </TouchableOpacity>
         </View>
-    );
+      </View>
+      <StatusBar style="auto" />
+      <View style={{flexDirection:'row',justifyContent:'flex-end'}}>
+          <Image  source={require('gomaoom/assets/greenBottom.png')}/>
+      </View>
+
+      </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    background:{
-        backgroundColor: '#FFFCF8',
-        flex:1,
-    },
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    desc:{
-        fontSize:25,
-        marginTop:30,
-        marginHorizontal:30,
-    },
-    btnContainer:{
-        // flexDirection:'column',
-        // justifyContent:'flex-end',
-        marginTop:110,
-        // backgroundColor:'pink',
-    },
-    loginBtn:{
-        backgroundColor:'#F1F1F1',
-        paddingHorizontal:30,
-        paddingVertical:7,
-        borderRadius:20,
-    },
+  background:{
+      backgroundColor: '#FFFCF8',
+      flex:1,
+  },
+  titleBox:{
+    flex:1,
+    // justifycontent: 'center',
+    // backgroundColor:'red'
+  },
+  mainTitle:{
+    // marginBottom:70,
+    fontSize:40,
+    
+    fontWeight:'bold',
+  },
+  container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  desc:{
+      fontSize:25,
+      marginTop:30,
+      marginHorizontal:30,
+  },
+  loginContainer:{
+    flex:3,
+    alignItems:'center',
+    // backgroundColor:'yellow',
+    // justifyContent: 'center'
+  },
+  btnContainer:{
+      // flexDirection:'column',
+      justifyContent:'center',
+      marginBottom:50,
+      // backgroundColor:'pink',
+  },
+  loginBtn:{
+      width:200,
+      backgroundColor:'#F1F1F1',
+      paddingHorizontal:30,
+      paddingVertical:7,
+      borderRadius:20,
+      marginTop:20,
+  },
+  loginInputContainer:{
+    // backgroundColor:'#F1F1F1',
+    borderBottomWidth:1,
+    borderColor:'grey',
+    paddingHorizontal:10,
+    paddingVertical:7,
+    borderRadius:30,
+    width:'70%',
+    marginBottom:15,
+  },
+  inputText:{
+    fontSize:25,
+    width:270,
+    paddingLeft:10,
+    // textAlign:'center',
+  },
 });
